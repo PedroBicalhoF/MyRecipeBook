92183ea9684d44d632ec57c2e66169d368191083
Pedro Bicalho <solpadetroi@gmail.com>
2026-07-23 21:26:26 -0300

    Initial Tests for UseCases and Validators

---
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,27 @@
+﻿using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Security;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Domain.Repositories.User;
+
+namespace UseCases.Tests.User.Register;
+
+public class RegisterUserAccountUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var useCase = CreateUseCase();
+    }
+
+    private RegisterUserAccountUseCase CreateUseCase()
+    {
+        var unitOfWork = IUnitOfWorkBuilder.Build();
+        var userWriteOnlyRepository = IUserWriteOnlyRepositoryBuilder.Build();
+        var userReadOnlyRepository = new IUserReadOnlyRepositoryBuilder().Build();
+        var passwordHasher = new IPasswordHasherBuilder().Build();
+        return new RegisterUserAccountUseCase(passwordHasher, userWriteOnlyRepository, userReadOnlyRepository, unitOfWork);
+    }
+}
---
+++ tests/Validators.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -0,0 +1,97 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exception;
+using Shouldly;
+
+namespace Validators.Tests.User.Register;
+
+public class RegisterUserAccountValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange - Configurações iniciais
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+
+        var validator = new RegisterUserAccountValidator();
+
+        //Act - Acoes para testar
+        var result = validator.Validate(request);
+
+        //Assert - Valor de Resposta esperado
+        result.IsValid.ShouldBeTrue();
+
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenPasswordIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsInvalid()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Email = "teste.com";
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
+    }
+}
