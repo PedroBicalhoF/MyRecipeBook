8e13bcc092133e80377f8008bc7fea25d4bf300d
Pedro Bicalho <solpadetroi@gmail.com>
2026-08-03 18:05:09 -0300

    Backup

--- tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -3,6 +3,7 @@ using CommonTestUtilities.Requests;
 using CommonTestUtilities.Security;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Domain.Repositories.User;
+using Shouldly;
 
 namespace UseCases.Tests.User.Register;
 
@@ -14,6 +15,31 @@ public class RegisterUserAccountUseCaseTests
         var request = RequestRegisterUserAccountJsonBuilder.Build();
 
         var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.ShouldNotBeNull();
+        result.Tokens.ShouldNotBeNull();
+        result.Name.ShouldBe(request.Name);
+        result.Tokens.AcessToken.ShouldBeNullOrEmpty();
+        result.Tokens.RefreshToken.ShouldBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase();
+
+        await useCase.Execute(request).ShouldThrowAsync();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_WhenEmailAlreadyExists()
+    {
+
     }
 
     private RegisterUserAccountUseCase CreateUseCase()
